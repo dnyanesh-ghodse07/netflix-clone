8dc7c3b43b8e1db9480993b102d4a906efe5eba5
dev_danny <dnyaneshghodse07@gmail.com>
2024-07-25 19:58:02 +0530

    navbar & banner

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,32 @@
-import './TitleCards.css';
+import "./TitleCards.css";
+import data  from "../../assets/thumbnails/thumbnails";
+import { useEffect, useRef } from "react";
 
 const TitleCards = () => {
-  return (
-    <div className='titlecards'>TitleCards</div>
-  )
-}
 
-export default TitleCards
+  const cardsRef = useRef();
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
+
+  useEffect(() => {
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  },[])
+
+  return (
+    <div className="title-cards">
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {data.map((card, index) => {
+          return <div key={index} className="card">
+            <img src={card.image} alt="" />
+          </div>;
+        })}
+      </div>
+      {/* {cardsData} */}
+    </div>
+  );
+};
+
+export default TitleCards;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,48 @@
-import './Home.css'
-import Navbar from '../../components/Navbar/Navbar';
-import Footer from '../../components/Footer/Footer';
-import TitleCards from '../../components/TitleCards/TitleCards';
+import Navbar from "../../components/Navbar/Navbar";
+// import Footer from '../../components/Footer/Footer';
+// import TitleCards from '../../components/TitleCards/TitleCards';
+import bannerImg from "../../assets/banner-img.webp";
+import bannerTitle from "../../assets/banner-title.webp";
+import playIcon from "../../assets/play.svg";
+import infoIcon from "../../assets/info.svg";
+import TitleCards from "../../components/TitleCards/TitleCards";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Navbar/>
+    <div className="home">
+      <Navbar />
+      <div className="hero">
+        <img src={bannerImg} alt="banner-img" className="banner-img" />
+        <div className="hero-caption">
+          <img src={bannerTitle} alt="caption" className="caption-img" />
+          <h3>Watch the Limited Series now</h3>
+          <p>
+            While the plague ravages 1348 Italy, the wealthy and their servants
+            gather for a wedding celebration, a bit of fun — and a little
+            mayhem.
+          </p>
+          <div className="hero-btns">
+            <button className="play">
+              <img src={playIcon} alt="play" />
+              Play
+            </button>
+            <button className="info">
+              <img src={infoIcon} alt="info" />
+              More Info
+            </button>
+          </div>
+          <TitleCards />
+        </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards />
+        <TitleCards />
+        <TitleCards />
+        <TitleCards />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
